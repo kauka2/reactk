7845e7f142e91e21d6e2ca8d948385d72e8dc976
kauka2 <103260795+kauka2@users.noreply.github.com>
2022-11-28 19:40:04 -0300

    Add files via upload

---
+++ reactk/src/pages/Home/index.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import carrousel from "assets/img/dragon.png";
+import * as S from "./styles";
+import { CardComponent } from "components";
+import { apiMessage } from "services/data";
+import { IMessageData } from "interfaces/message.interface";
+
+const Home = () => {
+  const [messages, setMessage] = useState<IMessageData[]>()
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await apiMessage.index()
+      setMessage(response.data)
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <S.Home>
+      <picture>
+        <img src={carrousel} alt="Imagens principais" />
+      </picture>
+      <aside>
+        {messages && messages.map((item) => <CardComponent key={item.id} dados={item} />)}
+      </aside>
+    </S.Home>
+  );
+};
+
+export default Home;
